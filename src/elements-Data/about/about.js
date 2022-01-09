c0eef278b07f2e5c8ed209ccb535ff9b62209896
~Gabriel <~gabriel.p1997@yahoo.com~>
2022-01-09 17:33:56 -0500

    update pdf resume

--- src/elements-Data/about/about.js
+++ src/elements-Data/about/about.js
@@ -3,7 +3,7 @@ import pdfEng from '../../assets/pdf/Gabriel-Oliveira.pdf'
 // eslint-disable-next-line import/no-anonymous-default-export
 export default  {
     eng: {
-        about_text: "I'm Front-End developer, with a passion in build interactives, responsiveness and friendly technologies for users. Focus on matching modern market technologies with deppling knowledge, resulting in a clean and objective code.",
+        about_text: "I'm Front-End developer, with a passion in build interactives, responsiveness and friendly technologies for users. Focus on matching modern market technologies with deep knowledge, resulting in a clean and objective code.",
         about_sub_text:'For more information on professional and academic background, access the resume at the button below.',
         button: 'Resume',
         about:'About',
